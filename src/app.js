ef64d1289a3ebf486eeb814cdc78135b889f26f6
Young Batimehin <benny.bat51@gmail.com>
2026-05-19 11:41:21 +0100

    Upload

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const robotRoutes = require("./api/robotRoutes");
+const cors = require('cors');
+const app = express();
+
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use("/api/robot", robotRoutes);
 
 module.exports = app;
