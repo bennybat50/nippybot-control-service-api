44397570f3e40881c7637aa52bce7616c5953043
Young Batimehin <benny.bat51@gmail.com>
2026-05-19 12:26:38 +0100

    sad

--- src/app.js
+++ src/app.js
@@ -3,14 +3,16 @@ const robotRoutes = require("./api/robotRoutes");
 const cors = require('cors');
 const app = express();
 
- 
-app.use(express.json());
+
  
 app.use(cors({
     origin: ['http://localhost:5173', 'https://botcontrol.nippybot.com/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
+
+app.use(express.json());
 
 app.use("/api/robot", robotRoutes);
 
