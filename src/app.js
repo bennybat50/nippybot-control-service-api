791ec15df93bcaa3ccbb1255e6b3712cd20a5d2d
Young Batimehin <benny.bat51@gmail.com>
2026-05-19 11:58:19 +0100

    update

--- src/app.js
+++ src/app.js
@@ -7,10 +7,6 @@ const app = express();
 app.use(express.json());
  
 app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-}, {
     origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
