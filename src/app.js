e4da8a481499ddb9bf684c575a1cbdf393b225ce
Young Batimehin <benny.bat51@gmail.com>
2026-05-19 12:15:36 +0100

    mn

--- src/app.js
+++ src/app.js
@@ -6,11 +6,11 @@ const app = express();
  
 app.use(express.json());
  
-app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-}));
+// app.use(cors({
+//     origin: 'http://localhost:5173',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true
+// }));
 
 app.use("/api/robot", robotRoutes);
 
